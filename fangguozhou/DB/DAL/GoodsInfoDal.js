b95d173292564f378bbd35eec0f1f05ffba95c4c
zengg <zengg@m_zengguiping>
2017-09-11 15:07:53 +0800

    修改数据库操作部分代码

--- fangguozhou/DB/DAL/GoodsInfoDal.js
+++ fangguozhou/DB/DAL/GoodsInfoDal.js
@@ -1,8 +1,8 @@
+var dbhelper = DBHelper.CreateNew("goods","存储商品信息");
 var GoodsInfoDal = {
 	CreateNew:function()
 	{
 		var goodsInfoDal;
-		var dbhelper = DBHelper.CreateNew("goods","存储商品信息");
 		//增
 		goodsInfoDal.add = function(var model)
 		{
@@ -36,14 +36,130 @@ var GoodsInfoDal = {
             });
 		};
 		
-		//查
-		goodsInfoDal.selectBySql=function(var sql,var resultsfunction)
+		return goodsInfoDal;
+	}
+}
+
+var ShopInfoDal = {
+	CreateNew:function()
+	{
+		var shopInfoDal;
+		//增
+		shopInfoDal.add = function(var model)
 		{
-			dbhelper.ExecuteSqlSql(sql,null,function (context, results) {
+			var add_goods_sql = 'INSERT INTO shopinfo(id,name,shopkeeper,phone,position) valuse(?,?,?,?,?)'
+			     ;
+			var paras = [model.id,model.name,model.shopkeeper,model.phone,model.position];
+			dbhelper.ExecuteSqlSql(add_goods_sql,paras);
+		};
+		//删
+		shopInfoDal.del=function(var id)
+		{
+			var del_goods_sql='DELETE FROM shopinfo WHERE id=？';
+			var paras =[id];
+			dbhelper.ExecuteSqlSql(del_goods_sql,[id]);
+		};
+		//改
+		shopInfoDal.update=function(var model)
+		{
+			var upd_goods_sql='UPDATE shopinfo SET name="?",shopkeeper="?",phone="?",position="?" WHERE id=?';
+			var paras = [model.name,model.shopkeeper,model.phone,model.position,model.id];
+			dbhelper.ExecuteSqlSql(upd_goods_sql,paras);
+		};
+		
+		//查
+		shopInfoDal.select=function(var where,var resultsfunction)
+		{
+			var sel_goods_sql='select * from shopinfo where ?';
+			var paras = [where];
+			dbhelper.ExecuteSqlSql(upd_goods_sql,paras,function (context, results) {
      			resultsfunction(results);
             });
 		};
 		
-		return goodsInfoDal;
+		return shopInfoDal;
+	}
+}
+
+var TypeInfoDal = {
+	CreateNew:function()
+	{
+		var typeInfoDal;
+		//增
+		typeInfoDal.add = function(var model)
+		{
+			var add_goods_sql = 'INSERT INTO typeinfo(id,name,remarks) valuse(?,?,?)'
+			     ;
+			var paras = [model.id,model.name,model.remarks];
+			dbhelper.ExecuteSqlSql(add_goods_sql,paras);
+		};
+		//删
+		typeInfoDal.del=function(var id)
+		{
+			var del_goods_sql='DELETE FROM typeinfo WHERE id=？';
+			var paras =[id];
+			dbhelper.ExecuteSqlSql(del_goods_sql,[id]);
+		};
+		//改
+		typeInfoDal.update=function(var model)
+		{
+			var upd_goods_sql='UPDATE typeinfo SET name="?",remarks="?" WHERE id=?';
+			var paras = [model.name,model.remarks,model.id];
+			dbhelper.ExecuteSqlSql(upd_goods_sql,paras);
+		};
+		
+		//查
+		typeInfoDal.select=function(var where,var resultsfunction)
+		{
+			var sel_goods_sql='select * from typeinfo where ?';
+			var paras = [where];
+			dbhelper.ExecuteSqlSql(upd_goods_sql,paras,function (context, results) {
+     			resultsfunction(results);
+            });
+		};
+		
+		return typeInfoDal;
+	}
+}
+
+
+var ImageInfoDal = {
+	CreateNew:function()
+	{
+		var imageInfoDal;
+		//增
+		imageInfoDal.add = function(var model)
+		{
+			var add_goods_sql = 'INSERT INTO imageinfo(id,goodsId,iconUrl,imageUrl,uploadTime) valuse(?,?,?,?,?)'
+			     ;
+			var paras = [model.id,model.goodsId,model.iconUrl,model.imageUrl,model.uploadTime];
+			dbhelper.ExecuteSqlSql(add_goods_sql,paras);
+		};
+		//删
+		imageInfoDal.del=function(var id)
+		{
+			var del_goods_sql='DELETE FROM imageinfo WHERE id=？';
+			var paras =[id];
+			dbhelper.ExecuteSqlSql(del_goods_sql,[id]);
+		};
+		//改
+		imageInfoDal.update=function(var model)
+		{
+			var upd_goods_sql='UPDATE imageinfo SET goodsId="?",iconUrl="?",imageUrl="?",uploadTime="?"  WHERE id=?';
+			var paras = [model.goodsId,model.iconUrl,model.imageUrl,model.uploadTime,model.id];
+			dbhelper.ExecuteSqlSql(upd_goods_sql,paras);
+		};
+		
+		//查
+		imageInfoDal.select=function(var where,var resultsfunction)
+		{
+			var sel_goods_sql='select * from imageinfo where ?';
+			var paras = [where];
+			dbhelper.ExecuteSqlSql(upd_goods_sql,paras,function (context, results) {
+     			resultsfunction(results);
+            });
+		};
+		
+		return imageInfoDal;
 	}
 }
--- fangguozhou/DB/Model/GoodsInfoModel.js
+++ fangguozhou/DB/Model/GoodsInfoModel.js
@@ -13,3 +13,49 @@ var GoodsInfoModel =
 	}
 	
 }
+
+//店铺表
+var ShopInfoModel = 
+{
+	CreateNew:  function ()
+	{
+		var shopInfoModel;
+		shopInfoModel.id = "";//店铺id
+		shopInfoModel.name = "";//店铺名
+		shopInfoModel.shopkeeper = "";//店主名
+		shopInfoModel.phone = "";//联系电话
+		shopInfoModel.position = "";//位置
+		return shopInfoModel;
+	}
+	
+}
+
+//分类表
+var TypeInfoModel = 
+{
+	CreateNew:  function ()
+	{
+		var typeInfoModel;
+		typeInfoModel.id = "";//分类id
+		typeInfoModel.name = "";//分类名
+		typeInfoModel.remarks = "";//备注
+		return typeInfoModel;
+	}
+	
+}
+
+//图片信息表
+var ImageInfoModel = 
+{
+	CreateNew:  function ()
+	{
+		var imageInfoModel;
+		imageInfoModel.id = "";//图片id
+		imageInfoModel.goodsId = "";//商品id
+		imageInfoModel.iconUrl = "";//缩略图路径
+		imageInfoModel.imageUrl = "";//原图片路径
+		imageInfoModel.uploadTime = "";//上传时间
+		return imageInfoModel;
+	}
+	
+}
